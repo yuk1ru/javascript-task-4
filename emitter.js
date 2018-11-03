91198da8285cfbc2468febddebe57d2390ad149b
Kirill Yuldashev <kirill.yuldashev@mail.ru>
2018-11-03 17:42:37 +0500

    Add solution without extra

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
@@ -12,32 +12,60 @@ const isStar = true;
  */
 function getEmitter() {
     return {
+        events: new Map(),
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {this}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!this.events.has(event)) {
+                this.events.set(event, new Map());
+            }
+            const contextMapping = this.events.get(event);
+            if (!contextMapping.has(context)) {
+                contextMapping.set(context, []);
+            }
+            const eventHandlers = contextMapping.get(context);
+            eventHandlers.push(handler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {this}
          */
         off: function (event, context) {
-            console.info(event, context);
+            const eventFilter = e =>
+                e === event || e.startsWith(`${event}.`);
+            const eventsToOff = [...this.events.keys()].filter(eventFilter);
+            eventsToOff.forEach(e => this.events.get(e).delete(context));
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {this}
          */
         emit: function (event) {
-            console.info(event);
+            const eventsToEmit = event.split('.').reduceRight((acc, _, i, arr) =>
+                acc.concat(arr.slice(0, i + 1).join('.')), []);
+
+            eventsToEmit.map(e => this.events.get(e))
+                .filter(e => Boolean(e))
+                .forEach(eventMapping =>
+                    eventMapping.forEach((handlers, context) =>
+                        handlers.forEach(handler => handler.call(context))));
+
+            return this;
         },
 
         /**
